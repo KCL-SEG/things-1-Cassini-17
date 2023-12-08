f6329528c291b788368b0397460cf5e07906551c
Cassini-17 <Alex.tsybulnikov@gmail.com>
2023-12-08 15:02:04 +0000

    add Things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Thing(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=120, blank=True, unique=False)
+    quantity = models.PositiveIntegerField(default=1,
+        validators=[MinValueValidator(0),MaxValueValidator(100)]
+    )
+
